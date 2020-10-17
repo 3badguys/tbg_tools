a7cdcce8eb91a7c338648249d926523b55986df5
ACT3 <chuiC456@163.com>
2020-10-17 21:02:34 +0800

    Add tbg_ssh.pl --help option for.

--- tbg_ssh/tbg_ssh.pl
+++ tbg_ssh/tbg_ssh.pl
@@ -10,16 +10,21 @@ use JSON::MaybeXS;
 use Data::Diver qw( Dive );
 use Getopt::Long;
 
-my $host;
-my $user;
-my $password;
-my $force_save;
+sub ssh_login($$$$);
+sub write_json_conf($$$);
+sub usage($);
+
 GetOptions(
-           'host=s'     => \$host,
-           'user=s'     => \($user = 'root'),
-           'password=s' => \$password,
-           'force-save' => \$force_save,
-          ) or die("Invalid options passed to $0\n");
+           'help'         => \(my $help),
+           'h|host=s'     => \(my $host),
+           'u|user=s'     => \(my $user = 'root'),
+           'p|password=s' => \(my $password),
+           'f|force-save' => \(my $force_save),
+          ) or usage(1);
+
+if ($help) {
+    usage(0);
+}
 
 my $tbg_ssh_json_file = "$HOME/.tbg/tbg_ssh/tbg_ssh.json";
 my $tbg_ssh_data = do {
@@ -29,7 +34,7 @@ my $tbg_ssh_data = do {
 };
 my $tbg_ssh_json = decode_json($tbg_ssh_data) // {};
 
-sub ssh_login {
+sub ssh_login($$$$) {
     my ($host, $user, $password, $force_save) = @_;
 
     if (!defined($host) || !defined($user)) {
@@ -53,7 +58,7 @@ sub ssh_login {
     die("system @cmd_args failed: $?");
 }
 
-sub write_json_conf {
+sub write_json_conf($$$) {
     my ($host, $user, $password) = @_;
 
     if (!defined($host) || !defined($user) || !defined($password)) {
@@ -69,6 +74,41 @@ sub write_json_conf {
     open(my $json_fh, ">", $tbg_ssh_json_file) or die($!);
     print $json_fh JSON::MaybeXS->new(utf8 => 1, pretty => 1)->encode($tbg_ssh_json);
     close($json_fh);
+}
+
+sub usage($) {
+    my $rc = shift;
+    my $msg = <<'_EOC_';
+Usage:
+    tbg_ssh.pl [optoins]
+
+Options:
+    --help                 Print this help.
+
+    -h <ip>
+    -host <ip>             Specify the host ip.
+
+    -u <user>
+    --user <user>          Specify the user.
+
+    -p <password>
+    --password <password>  Specify the password.
+
+    -f
+    --force-save           Force to save the password.
+
+Examples:
+    tbg_ssh.pl --help
+    tbg_ssh.pl -h 192.168.56.67 -u root -p 456567 -f
+_EOC_
+
+    if ($rc == 0) {
+        print $msg;
+        exit(0);
+    }
+
+    warn($msg);
+    exit($rc);
 }
 
 ssh_login($host, $user, $password, $force_save);
